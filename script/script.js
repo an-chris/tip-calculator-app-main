d9e599bb6b3597a393ab9ec0aee1078ca60af2ca
Chris An <an-chris@outlook.com>
2021-07-22 15:43:36 -0500

    changes to element selecting variables

--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
+// Input field
 const bill = document.querySelector('.input__bill');
-const billInput  = bill.value;
-console.log(bill)
-const peopleInput = document.querySelector('.amount__people').value;
-const tipInput = document.querySelector('.input__tip-custom').value;
-// buttons
+const billInput = bill.value;
+const people = document.querySelector('.amount__people');
+const peopleInput = people.value
+const tip = document.querySelector('.input__tip-custom');
+const tipInput = tip.value
+// Buttons
 const fivePercent = document.getElementById('five__percent');
 const tenPercent = document.getElementById('five__percent');
 const fifteenPercent = document.getElementById('five__percent');
@@ -11,9 +13,12 @@ const twentyFivePercent = document.getElementById('five__percent');
 const fiftyPercent = document.getElementById('five__percent');
 const reset = document.querySelector('.amount__calculation-reset');
 
+// Get bill value and display into total amount
 bill.addEventListener('change', function (e) {
-  // billInput = bill.value;
-  // console.log(billInput);
-  console.log(e.target.value)
+  // e is the event, target is the element that triggered the event which was trigger by the input,
+  // and the value is the value of the input element
+  console.log(e.target.value);
   document.querySelector('.amount__calculation-total-total').textContent = "$" + e.target.value;
+  console.log(e);
 })
+
