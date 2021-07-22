9bd22ebe5fa08079f872516abaa2e4fec12db48b
Chris An <an-chris@outlook.com>
2021-07-21 20:07:34 -0500

    Make variables that select elements
    -create ids for html elements with the same class names

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// Selecting elements
+// inputs
+const billInput = document.querySelector('.input__bill').value;
+const peopleInput = document.querySelector('.amount__people').value;
+const tipInput = document.querySelector('.input__tip-custom').value;
+// buttons
+const fivePercent = document.getElementById('five__percent');
+const tenPercent = document.getElementById('five__percent');
+const fifteenPercent = document.getElementById('five__percent');
+const twentyFivePercent = document.getElementById('five__percent');
+const fiftyPercent = document.getElementById('five__percent');
