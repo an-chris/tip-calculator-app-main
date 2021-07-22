f385c0a36f7699f9821fb51a80b8cc664cbc5c87
Chris An <an-chris@outlook.com>
2021-07-22 00:14:53 -0500

    listen for changes in the bill input and display it onto the total amount

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
-// Selecting elements
-// inputs
-const billInput = document.querySelector('.input__bill').value;
+const bill = document.querySelector('.input__bill');
+const billInput  = bill.value;
+console.log(bill)
 const peopleInput = document.querySelector('.amount__people').value;
 const tipInput = document.querySelector('.input__tip-custom').value;
 // buttons
@@ -10,3 +10,10 @@ const fifteenPercent = document.getElementById('five__percent');
 const twentyFivePercent = document.getElementById('five__percent');
 const fiftyPercent = document.getElementById('five__percent');
 const reset = document.querySelector('.amount__calculation-reset');
+
+bill.addEventListener('change', function (e) {
+  // billInput = bill.value;
+  // console.log(billInput);
+  console.log(e.target.value)
+  document.querySelector('.amount__calculation-total-total').textContent = "$" + e.target.value;
+})
